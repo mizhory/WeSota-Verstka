eba7f69d1dc7a1833090827183fbd1f2828f0031
MizhoryDevil-Oper <mizhory@gmail.com>
2020-11-21 18:51:10 +0300

    #Verstka v1.3

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let menuIcons = document.querySelectorAll(".menu-icon");
+let sbLinks = document.querySelectorAll(".sb-link");
+
+for(let sbLink of sbLinks){
+
+for (let icon of menuIcons){
+
+  // if(icon.classList.contains('statistic') && sbLink.classList.contains('active')){
+  //     icon.src = "icons/menu-color/statistic.png";
+  //   } else if(icon.classList.contains('finance') && elm.classList.contains('active')){
+  //     icon.src = "icons/menu-color/finance.png";
+  //   } else if(icon.classList.contains('invite') && elm.classList.contains('active')){
+  //     icon.src = "icons/menu-color/invite.png";
+  //   }else if(icon.classList.contains('platform') && elm.classList.contains('active')){
+  //     icon.src = "icons/menu-color/platform.png";
+  //   }else if(icon.classList.contains('message') && elm.classList.contains('active')){
+  //     icon.src = "icons/menu-color/message.png";
+  //   } else{
+  //     icon.src = "icons/menu-color/settings.png";
+  // };
+};
+};
